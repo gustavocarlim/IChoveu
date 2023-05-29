3151a36b076255ce858dc418829d1f02bd02e89a
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-05-29 18:51:56 -0300

    comitando

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,26 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = '05f7b4d9cdc3455ea75185621232505';
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+  const data = await response.json();
+
+  if (data.length === 0) {
+    window.alert('Nenhuma cidade encontrada');
+  }
+
+  return console.log(data);
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
-};
+
+export const getWeatherByCity = async (cityURL) => {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
+    const data = await response.json();
+  
+    const weatherData = {
+      temp: data.current.temp_c,
+      condition: data.current.condition.text,
+      icon: data.current.condition.icon
+    };
+  
+    return console.log (weatherData);
+  };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { handleSearch } from './helpers/pageFunctions';
+import { handleSearch, createCityElement } from './helpers/pageFunctions';
 import './style.css';
+
+import { searchCities, getWeatherByCity } from './helpers/weatherAPI.js';
 
 document.getElementById('search-form')
   .addEventListener('submit', handleSearch);
@@ -8,3 +10,33 @@ document.getElementById('close-forecast')
   .addEventListener('click', () => {
     document.getElementById('forecast-container').classList.add('hidden');
   });
+
+const searchButton = document.getElementById('search-button');
+const searchTerm = document.getElementById('search-term');
+const citiesList = document.getElementById('cities');
+
+searchButton.addEventListener('click', async () => {
+  const term = searchTerm.value;
+
+  const cities = await searchCities(term);
+
+  citiesList.innerHTML = '';
+
+  if (cities.length === 0) {
+    window.alert('Nenhuma cidade encontrada');
+    return;
+  }
+
+  cities.forEach(async (city) => {
+    const weatherData = await getWeatherByCity(city.url);
+    const cityElement = createCityElement({
+      name: city.name,
+      country: city.country,
+      temp: weatherData.temp,
+      condition: weatherData.condition,
+      icon: weatherData.icon,
+      url: city.url,
+    });
+    citiesList.appendChild(cityElement);
+  });
+});
