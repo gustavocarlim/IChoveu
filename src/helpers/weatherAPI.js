e11da47ce55d30e14209143e1ddc927e28b9346f
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-05-30 13:29:35 -0300

    primeiro requisito

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,26 +1,25 @@
-const TOKEN = '05f7b4d9cdc3455ea75185621232505';
+const TOKEN = import.meta.env.VITE_TOKEN;
 
 export const searchCities = async (term) => {
   const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
   const data = await response.json();
 
-  if (data.length === 0) {
+  if (data.length === 0 || !term) {
     window.alert('Nenhuma cidade encontrada');
   }
 
-  return console.log(data);
+  return data;
 };
 
-
 export const getWeatherByCity = async (cityURL) => {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
-    const data = await response.json();
-  
-    const weatherData = {
-      temp: data.current.temp_c,
-      condition: data.current.condition.text,
-      icon: data.current.condition.icon
-    };
-  
-    return console.log (weatherData);
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
+  const data = await response.json();
+
+  const weatherData = {
+    temp: data.current.temp_c,
+    condition: data.current.condition.text,
+    icon: data.current.condition.icon,
   };
+
+  return weatherData;
+};
